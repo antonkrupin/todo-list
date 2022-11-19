988b565426b383edf96ad32dbe2bfaddc2d90f1d
Anton Krupin <krupin_anton@mail.ru>
2022-11-19 14:35:08 +0300

    add import TodoList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './comonents/TodoList';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todolist d-flex flex-column text-center m-5">
+      <h1 className="text-danger">Todo List</h1>
+      <TodoList />
     </div>
-  );
+  )
 }
 
 export default App;
