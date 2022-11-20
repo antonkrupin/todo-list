b2cb9eee0b674c9f6adfb154f32f3aafb7215211
Anton Krupin <krupin_anton@mail.ru>
2022-11-20 12:31:00 +0300

    add TodoItem render

--- src/comonents/TodoList.jsx
+++ src/comonents/TodoList.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import TodoItem from './TodoItem';
 
 const TodoList = () => {
+  const todos = useSelector((state) => state.todos.todos);
+
   return (
     <div className="d-flex flex-column">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {todos.map((todo, index) => (
+        <TodoItem
+          key={index}
+          todo={todo}
+        />
+      ))}
     </div>
   )
 };
