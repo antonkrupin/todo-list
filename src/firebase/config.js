6141465f0b0e1bd0773d7bbad2874fc1b7c09f34
Anton Krupin <krupin_anton@mail.ru>
2022-11-20 12:59:43 +0300

    create config.js for firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAeZr8pgURt3hMf8HBzUAtxm33a1wovf-A",
+  authDomain: "test-list-c404a.firebaseapp.com",
+  projectId: "test-list-c404a",
+  storageBucket: "test-list-c404a.appspot.com",
+  messagingSenderId: "507116778720",
+  appId: "1:507116778720:web:a83c226603d64bff4c4c85"
+};
+
+export default firebaseConfig;
