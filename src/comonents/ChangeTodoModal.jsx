6f7fc04df0cc71281fd8b14370494034d088164f
Anton Krupin <krupin_anton@mail.ru>
2022-11-21 21:02:10 +0300

    change markup

--- src/comonents/ChangeTodoModal.jsx
+++ src/comonents/ChangeTodoModal.jsx
@@ -1,71 +1,81 @@
-import { useState, useRef, useEffect } from 'react';
+import { useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { Modal, Button } from 'react-bootstrap';
 
-import { changeTodoModalShow } from '../store/modalsSlice'
+import { changeTodo } from '../store/todoSlice';
+import { changeTodoModalShow } from '../store/modalsSlice';
 
 
 const ChangeTodoModal = (props) => {
 	const dispatch = useDispatch();
 
-	/*const ref = useRef();
-
-	const { socket } = props;
-	
-	const channel = useSelector((state) => state.channels.channelForRename);
-
-	const channelsNames = useSelector((state) => state.channels.channels).map(({name}) => name);*/
+	const titleRef = useRef();
+	const descriptionRef = useRef();
+	const dateRef = useRef();
 	
 	const isChangeTodoShow = useSelector((state) => state.modals.isChangeTodoShow);
 
 	const todoKey = useSelector((state) => state.todos.todoForChange[0]);
 	const todoForChange = useSelector((state) => state.todos.todoForChange[1]);
 	
-	useEffect(() => {
-		console.log(todoKey, todoForChange);
-	}, [isChangeTodoShow])
-
-	/*const [name, setChannelName] = useState(null);
-
-	const [error, setError] = useState('');
-
-	const { channelStatus } = useSelector((state) => state.channels);*/
-
-	/*const renameChannelHandler = (e) => {
-		e.preventDefault();
-		dispatch(setChannelStatus('renaming'));
-		if (!_.includes(channelsNames, name)) {
-			socket.emit('renameChannel', { id: channel.id, name });
-			setChannelName('');
-			setError('');
-			dispatch(renameChannelModalShow());
-		} else {
-			dispatch(setChannelStatus(null));
-			//const className = cn('form-control', 'is-invalid');
-			ref.current.className = changeClassName('form-control is-invalid');
-			setError(i18n.t('errors.channels.renameChannel'));
-		}
-	}*/
-
 	const cancelChangeTodo = () => {
+		dispatch(changeTodoModalShow());
+	}
+
+	const saveChanges = (e) => {
+		e.preventDefault();
+		const changedTodo = {
+			title: titleRef.current.value,
+			description: descriptionRef.current.value,
+			date: dateRef.current.value,
+		}
+		dispatch(changeTodo({todoKey, changedTodo}));
 		dispatch(changeTodoModalShow());
 	}
 
 	return (
 		<>
-		<Modal show={isChangeTodoShow} onHide={cancelChangeTodo} >
+		<Modal show={isChangeTodoShow} onHide={cancelChangeTodo}>
 			<Modal.Header closeButton >
 				<Modal.Title>Изменить задачу</Modal.Title>
 			</Modal.Header>
 			<Modal.Body>
-				...
+				{todoForChange && (
+					<>
+						<label className="pb-2" htmlFor="todoTitle">Заголовок задачи</label>
+						<input
+							className="form-control pb-2"
+							name="todoTitle"
+							ref={titleRef}
+							defaultValue={todoForChange.title}
+							required
+						/>
+						<label className="pb-2" htmlFor="todoDescription">Описание задачи</label>
+						<textarea
+							className="form-control pb-2"
+							name="todoDescription" id="" cols="30" rows="5"
+							defaultValue={todoForChange.description}
+							ref={descriptionRef}
+						/>
+						<label htmlFor="todoDate">Дата завершения</label>
+						<input
+							className="w-50"
+							type="date"
+							name="todoDate"
+							defaultValue={todoForChange.date}
+							ref={dateRef}
+							required
+						/>
+					</>
+				)}
+				
 			</Modal.Body>
 			<Modal.Footer>
           <Button variant="secondary" onClick={cancelChangeTodo}>
             Отменить
           </Button>
-          <Button variant="primary">
+          <Button variant="primary" onClick={(e) => saveChanges(e)}>
             Сохранить
           </Button>
         </Modal.Footer>
