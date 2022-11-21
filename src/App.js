4983f97da0d45df417d86cf27d0e734cfbd68ef7
antonkrupin <krupin_anton@mail.ru>
2022-11-21 16:02:49 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { fetchTodos, setTodos } from './store/todoSlice';
-
 import TodoList from './comonents/TodoList';
 import AddTodo from './comonents/AddTodo';
+
 
 import './App.css';
 
 const App = () => {
-  const dispatch = useDispatch();
-  const todos = useSelector((state) => state.todos.todos);
-  
-
-  useEffect(() => {
-    console.log('start useeffect')
-    dispatch(fetchTodos());
-    console.log('end useeffect')
-    /*const test = dispatch(fetchTodos());
-    console.log(test)
-    test.then((data) => console.log(data.payload));*/
-    //dispatch(setTodos());
-    console.log('todos', todos);
-  }, []);
-
   return (
     <div className="todolist d-flex flex-column m-5">
       <h1 className="text-danger">Todo List</h1>
