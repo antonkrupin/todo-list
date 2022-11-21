6dfbe12da3185952eb5175df3f8c2d2af9613eb1
antonkrupin <krupin_anton@mail.ru>
2022-11-21 16:04:05 +0300

    change

--- src/comonents/TodoList.jsx
+++ src/comonents/TodoList.jsx
@@ -1,22 +1,35 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getTodos } from '../store/todoSlice';
+import React from 'react';
 
 import TodoItem from './TodoItem';
 
+import { ref } from 'firebase/database';
+import { useList } from 'react-firebase-hooks/database';
+import { database } from '../firebase/firebase';
+
+import ChangeTodoModal from './ChangeTodoModal';
+
 const TodoList = () => {
-  const dispatch = useDispatch();
-  const todos = useSelector((state) => state.todos.todos);
-  console.log('todos', todos);
-  
+	const [snapshots, loading, error] = useList(ref(database, 'todos'));
+    
   return (
     <div className="d-flex flex-column">
-      {/*todos.map((todo, index) => (
-        <TodoItem
-          key={index}
-          todo={todo}
-        />
-      ))*/}
+      {error && <strong>Error: {error}</strong>}
+        {loading && 
+					<div className="d-flex justify-content-center">
+						<h3 className="text-success">Загрузка списка задач</h3>
+					</div>}
+        {!loading && snapshots && (
+          <>
+						{snapshots.map((elem) => (
+							<TodoItem
+							key={elem.key}
+							todo={elem.val()}
+							id={elem.key}
+						/>
+						))}
+          </>
+        )}
+			<ChangeTodoModal />
     </div>
   )
 };
