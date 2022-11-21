d8e654e2c2431dd8286ca0dbcb2f8d7faa41ddd8
antonkrupin <krupin_anton@mail.ru>
2022-11-21 16:01:32 +0300

    change todoSlice

--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -2,134 +2,37 @@ import { createSlice, createAsyncThunk, current } from '@reduxjs/toolkit';
 
 import { db } from '../firebase/firebase';
 import { collection, addDoc, getDocs } from 'firebase/firestore';
-import { ref, onValue, get, child } from 'firebase/database';
+//import { ref, onValue, get, child } from 'firebase/database';
 import { database } from '../firebase/firebase';
-
-
-export const fetchTodos = createAsyncThunk(
-  'todos/fetchTodos',
-    async function() {
-      const refdb = ref(database);
-      const todos1 = [];
-      console.log('start')
-      const test = await get(child(refdb, 'todos')).then((snapshot) => {
-        if (snapshot.exists()) {
-          const data = snapshot.val();
-          const keys = Object.keys(data);
-          keys.forEach((key) => {
-            const todo = data[key];
-            todo.key = key;
-            todos1.push(todo);
-          });
-          return todos1;
-        } else {
-          console.log("No data available");
-        }
-      });
-      console.log('test', test);
-      console.log('finish');
-      return test;
-    }
-  /*async function() {
-    const response = ref(database, 'todos');
-    const todos = [];
-    await onValue(response, (snapshot) => {
-      const data = snapshot.val();
-      const keys = Object.keys(data);
-      keys.forEach((key) => {
-        todos.push(data[key]);
-      });
-    })
-    return todos;
-  }*/
-  /*async function() {
-    const refdb = ref(database);
-      const test = await get(child(refdb, 'todos')).then((snapshot) => {
-        if (snapshot.exists()) {
-          return snapshot.val();
-          //console.log(snapshot.val());
-        } else {
-          console.log("No data available");
-        }
-      });
-      console.log(test);
-  }*/
-)
+import { ref, set, push, remove, update } from 'firebase/database';
 
 const todoSlice = createSlice({
   name: 'todos',
   initialState: {
-    todos: [],
+    todoForChange: [],
   },
   reducers: {
     addTodo: (state, action) => {
-      state.todos = [action.payload, ...state.todos];
+      try {
+				push(ref(database, 'todos'), action.payload);
+			} catch(e) {
+				throw e;
+			}
     },
     toggleComplete: (state, action) => {
-
+			const {id, completed } = action.payload;
+			update(ref(database, 'todos/' + id), {
+				completed: !completed,
+			});
     },
     removeTodo: (state, action) => {
-
+			const id = action.payload;
+			remove(ref(database, 'todos/' + id));
     },
-    addTodoFb: async (state, action) => {
-      /*try {
-        const test = await addDoc(collection(db, 'todos'), action.payload);
-        console.log(test);
-      } catch(e) {
-        throw e;
-      }*/
-    },
-    setTodos: async (state, action) => {
-      const refdb = ref(database);
-      const todos = [];
-      const test = await get(child(refdb, 'todos')).then((snapshot) => {
-        if (snapshot.exists()) {
-          const data = snapshot.val();
-          const keys = Object.keys(data);
-          keys.forEach((key) => {
-            const todo = data[key];
-            todo.key = key;
-            state.todos = [todo, ...state.todos];
-            todos.push(todo);
-          });
-          return todos;
-        } else {
-          console.log("No data available");
-        }
-      })
-      console.log('start');
-      console.log(test);
-      console.log('state', state.todos);
-      //state.todos.push(test);
-      console.log('state', state.todos);
-      console.log('finished');
-      /*const response = ref(database, 'todos');
-      const todos = [];
-      onValue(response, (snapshot) => {
-        const data = snapshot.val();
-        const keys = Object.keys(data);
-        keys.forEach((key) => {
-          todos.push(data[key]);
-        });
-      });*/
-      //state.todos = todos;
-      //todos.then((data) => state.todos = data);
-    },
-    extraReducers: {
-      [fetchTodos.pending]: (state) => {
-        console.log('pending');
-      },
-      [fetchTodos.fulfilled]: (state, action) => {
-        console.log('resolved');
-        //state.todos.push(action.payload);
-        //state.todos = action.payload;
-        //console.log(action.payload);
-        //state.todos = action.payload;
-      },
-      [fetchTodos.rejected]: (state) => {
-        console.log('rejected');
-      },
-    },
+		setTodoForChange: (state, action) => {
+			const { id, todo } = action.payload;
+			state.todoForChange = [id, todo];
+		},
   }
 });
 
@@ -137,8 +40,7 @@ export const {
   addTodo,
   toggleComplete,
   removeTodo,
-  addTodoFb,
-  
+	setTodoForChange,
 } = todoSlice.actions;
 
 export default todoSlice.reducer;
