039ab1e89e20d2bc0699ff5079f334291e13f27b
Anton Krupin <krupin_anton@mail.ru>
2022-11-21 21:00:56 +0300

    remove filter reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import filterReducer from './filterSlice';
 import todoReducer from './todoSlice';
 import modalsReducer from './modalsSlice';
 
 export default configureStore({
   reducer: {
-    filter: filterReducer,
     todos: todoReducer,
 		modals: modalsReducer,
-  },
+  }
 });
