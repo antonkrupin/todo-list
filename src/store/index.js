4392bf37cf5164aa54de9ce9876f4934163e572a
antonkrupin <krupin_anton@mail.ru>
2022-11-21 16:02:04 +0300

    add import modalsReducer from './modalsSlice';

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import filterReducer from './filterSlice';
 import todoReducer from './todoSlice';
+import modalsReducer from './modalsSlice';
 
 export default configureStore({
   reducer: {
     filter: filterReducer,
     todos: todoReducer,
+		modals: modalsReducer,
   },
 });
