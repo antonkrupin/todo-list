3db8230785d8c95c10ca0bdedfb8d2ab5f588961
antonkrupin <krupin_anton@mail.ru>
2022-11-22 11:43:41 +0300

    create uploadFile function

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,8 @@
 import * as fb from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+//import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
+
+import { getStorage, ref, getDownloadURL, uploadBytes } from 'firebase/storage';
 
 import firebaseConfig from "./config";
 
@@ -10,5 +12,24 @@ export const firebase = fb.initializeApp(firebaseConfig);
  * Data base ref
  */
 export const database = getDatabase(firebase);
+
+/**
+ * upload file to filestorage
+ * @param {File} file uploaded file
+ * @param {function} setFilePath function for adding uploaded files paths
+ */
+export const uploadFile = (file, setFilePath) => {
+	const storage = getStorage();
+	const filesRef = ref(storage, 'files/' + file.name);
+	
+	uploadBytes(filesRef, file)
+	.then((snapshot) => {
+		getDownloadURL(snapshot.ref).then((downloadURL) => {
+			setFilePath((prev) => {
+				return [...prev, [file.name, downloadURL]];
+			});
+		})
+	});
+}
 
 
